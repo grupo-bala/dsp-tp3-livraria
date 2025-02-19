c3703b0e00a28f26e9cb2b6dbcbd1a422aa42364
Henok Teixeira <henokteixeira@gmail.com>
2025-02-19 15:01:42 -0300

    feat: add fastAPI app and setting database info

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from .routes.customer import customer_router
+from .database.connection import connect_db, close_db
 
-app = FastAPI()
+DATABASE_URL = "mongodb://localhost:27017"
+DATABASE_NAME = "meu_banco"
 
+@asynccontextmanager
+async def db_lifespan(app):
+    await connect_db(DATABASE_URL, DATABASE_NAME)
+    yield
+    await close_db()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app = FastAPI(lifespan=db_lifespan)
+
+app.include_router(customer_router)
