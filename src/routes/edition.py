d8c88efda0490c0f674b422113b8dddf11ad75a0
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2025-02-20 23:46:32 -0300

    feat: add route to calculate sum of editions prices

--- src/routes/edition.py
+++ src/routes/edition.py
@@ -1,13 +1,15 @@
 from typing import Annotated, Optional
 from fastapi import APIRouter, HTTPException, Query
-from ..models import Edition
+from ..models import Edition, EditionsPriceSum
 from .utils import get_response_object, generate_filters
 
 edition_router = APIRouter(prefix="/editions", tags=["Editions"])
 
+
 @edition_router.post("")
 async def create_edition(edition: Edition):
     return {"data": await edition.insert()}
+
 
 @edition_router.get("")
 async def get_editions(
@@ -27,7 +29,7 @@ async def get_editions(
     if sort_by:
         sort_direction = 1 if sort_order == "asc" else -1
         query = query.sort((sort_by, sort_direction))
-    
+
     total_count = await query.count()
     editions = await query.skip((page - 1) * size).limit(size).to_list()
 
@@ -54,3 +56,15 @@ async def delete_edition(id: str):
 
     await edition.delete()
     return {"message": "Edition deleted successfully"}
+
+
+@edition_router.get("price-sum")
+async def price_sum():
+    result = (
+        await Edition.find(fetch_links=True).project(EditionsPriceSum).first_or_none()
+    )
+
+    if result is None:
+        return {"sum": 0}
+
+    return {"sum": result.total}
